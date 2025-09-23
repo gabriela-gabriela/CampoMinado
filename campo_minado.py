1cdd976585876577a59fef7e16c4c05f9e077b92
gabriela-gabriela <gabrielaramalhodasilva@gmail.com>
2025-09-23 02:46:31 -0300

    Update campo_minado.py

--- campo_minado.py
+++ campo_minado.py
@@ -4,6 +4,77 @@ import curses
 from curses import wrapper
 
 
+def movimentar_cursor(tecla, cursor_y, cursor_x, altura, largura):
+    if tecla == curses.KEY_DOWN and cursor_y < altura:
+        cursor_y += 1
+    elif tecla == curses.KEY_UP and cursor_y > 1:
+        cursor_y -= 1
+    elif tecla == curses.KEY_LEFT and cursor_x > 1:
+        cursor_x -= 1
+    elif tecla == curses.KEY_RIGHT and cursor_x < largura:
+        cursor_x += 1
+    return cursor_y, cursor_x
+
+def dados_por_dificuldade(opcao_escolhida):
+    if opcao_escolhida == "Fácil":
+        return 8, 10, 10
+
+    elif opcao_escolhida == "Médio":
+        return 14, 18, 40
+
+    elif opcao_escolhida == "Difícil":
+        return 20, 24, 100
+
+def jogar(altura, largura, bombas, tela):
+    jogo = campo.Campo(altura, largura, bombas)
+    casas_vazias = altura * largura - bombas
+    janela_jogo = tela.criar_janela(altura, largura, jogo.campo_de_jogo)
+    cursor_y, cursor_x = 1, 1 #posicoes do "cursor" que vai facilitar o jogo
+    tela.atualizar_janela(janela_jogo, cursor_y, cursor_x, jogo.campo_de_jogo)
+
+
+    #fazer uma primeira jogada separado que cava 0 obrigatoriamente
+    while True:
+        tecla = janela_jogo.getch()
+        if tecla == ord("q"):
+            break #aqui vai pra segunda jogada sendo que era pra voltar pra o menu
+        elif tecla == ord("c"):
+            jogo.criar_campos(cursor_y - 1, cursor_x - 1)
+            casas_cavadas = jogo.cavar(cursor_y - 1, cursor_x - 1)
+            casas_vazias -= casas_cavadas
+            break
+        elif tecla in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
+            cursor_y, cursor_x = movimentar_cursor(tecla, cursor_y, cursor_x, altura, largura)
+
+            tela.atualizar_janela(janela_jogo, cursor_y, cursor_x, jogo.campo_de_jogo)
+
+    # esse loop aqui embaixo é pra as jogadas a partir da segunda
+    while True:
+        tela.atualizar_janela(janela_jogo, cursor_y, cursor_x, jogo.campo_de_jogo)
+
+        tecla = janela_jogo.getch()
+
+        if tecla == ord("q"):
+            break
+
+        elif tecla == ord("c"):
+            casas_cavadas = jogo.cavar(cursor_y - 1, cursor_x - 1)
+            if casas_cavadas == "gameover":
+                tela.game_over()
+                break
+
+            casas_vazias -= casas_cavadas
+            if casas_vazias == 0:
+                tela.vitoria()
+                break
+
+        elif tecla == ord("m"):
+            jogo.marcar_bomba(cursor_y - 1, cursor_x - 1)
+
+        elif tecla in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
+            cursor_y, cursor_x = movimentar_cursor(tecla, cursor_y, cursor_x, altura, largura)
+
+
 #refazendo tudo:
 def main(stdscr):
     tela = interface.Interface(stdscr)
@@ -12,69 +83,17 @@ def main(stdscr):
     # mostrando o menu para o jogador com as opcoes:
     while True:
         opcao_escolhida = tela.menu(stdscr)
-        if opcao_escolhida == "Fácil":
-            altura = 8
-            largura = 10
-            bombas = 10
-
-        elif opcao_escolhida == "Médio":
-            altura = 14
-            largura = 18
-            bombas = 40
-
-        elif opcao_escolhida == "Difícil":
-            altura = 20
-            largura = 24
-            bombas = 40
-
-        elif opcao_escolhida == "Ajuda e Créditos":
-            stdscr.clear()
-            ajudacreditos = "em andamento, feito por Gabriela e Lara"
-            stdscr.addstr(curses.LINES // 2 - len(ajudacreditos) // 2, curses.COLS // 2)
-            stdscr.refresh()
-            #colocar algo pra sair daqui
+        if opcao_escolhida == "Ajuda e Créditos":
+            tela.ajuda_creditos()
+            continue
 
         elif opcao_escolhida == "Sair":
             break
 
-        jogo = campo.Campo(altura, largura, bombas)
-        jogo.criar_campos()
-        janela_jogo = tela.criar_janela(altura, largura, jogo.campo_de_jogo)
-        cursor_y, cursor_x = 1, 1 #posicoes do "cursor" que vai facilitar o jogo
-        tela.atualizar_janela(janela_jogo, cursor_y, cursor_x, jogo.campo_de_jogo)
+        else:
+            altura, largura, bombas = dados_por_dificuldade(opcao_escolhida)
 
-        #fazer uma primeira jogada separado que cava 0 obrigatoriamente
-
-
-        casas_vazias = altura * largura - bombas
-        while True:
-            tela.atualizar_janela(janela_jogo, cursor_y, cursor_x, jogo.campo_de_jogo)
-
-            tecla = janela_jogo.getch()
-
-            if tecla == ord("q"):
-                 break
-
-            elif tecla == curses.KEY_DOWN and cursor_y < altura:
-                cursor_y += 1
-            elif tecla == curses.KEY_UP and cursor_y > 1:
-                cursor_y -= 1
-            elif tecla == curses.KEY_LEFT and cursor_x > 1:
-                cursor_x -= 1
-            elif tecla == curses.KEY_RIGHT and cursor_x < largura:
-                cursor_x += 1
-
-            elif tecla == ord("c"):
-                casas_cavadas = jogo.cavar(cursor_y - 1, cursor_x - 1)
-                if casas_cavadas == "gameover":
-                    tela.game_over()
-                    
-                casas_vazias -= casas_cavadas
-                if casas_vazias == 0:
-                    tela.vitoria()
-
-            elif tecla == ord("m"):
-                jogo.marcar_bomba(cursor_y - 1, cursor_x - 1)
+        jogar(altura, largura, bombas, tela)
 
 
 curses.wrapper(main)
