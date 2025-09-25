5e5408328bf77072e317aa099d6cdc16cc5be6f4
gabriela-gabriela <gabrielaramalhodasilva@gmail.com>
2025-09-25 01:20:44 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,8 +1,18 @@
 import curses
+import time
+
 
 class Interface:
     def __init__(self, stdscr):
         self.stdscr = stdscr
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        self.branco_preto = curses.color_pair(1)
+        curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        self.azul_preto = curses.color_pair(2)
+        curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+        self.vermelho_preto = curses.color_pair(3)
+        curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        self.verde_preto = curses.color_pair(4)
 
     def menu(self, stdscr):
         self.stdscr.clear()
@@ -40,7 +50,7 @@ class Interface:
     def criar_janela(self, altura, largura, campo_de_jogo):
         # aumentando um pouco a altura e largura baseado no campo pra poder caber dentro da borda
         altura_janela = altura + 2
-        largura_janela = largura + 2
+        largura_janela = largura * 2 + 2
         y_janela = (curses.LINES // 2) - (altura_janela // 2)
         x_janela = (curses.COLS // 2) - (largura_janela // 2)
         janela = curses.newwin(altura_janela, largura_janela, y_janela, x_janela)
@@ -56,9 +66,18 @@ class Interface:
                 casa = campo_de_jogo[lin][col]
                 if casa == None:
                     casa = "o"
-                janela.addch(lin + 1, col + 1, casa)
+                if casa == "o":
+                    janela.addstr(lin + 1, col * 2 + 1, "  ", curses.A_REVERSE)
+                elif casa == "x":
+                    janela.addstr(lin + 1, col * 2 + 1, " X", self.vermelho_preto | curses.A_REVERSE)
+                elif casa == "1":
+                    janela.addstr(lin + 1, col * 2 + 1, " 1", self.azul_preto)
+                elif casa == "2":
+                    janela.addstr(lin + 1, col * 2 + 1, " 2", self.verde_preto)
+                else:
+                    janela.addstr(lin + 1, col * 2 + 1, " " + casa, self.vermelho_preto)
 
-        janela.addch(cursor_y, cursor_x, "@", curses.A_REVERSE)
+        janela.addstr(cursor_y, cursor_x, "@@", curses.A_REVERSE)
         janela.refresh()
 
     def ajuda_creditos(self):
@@ -68,6 +87,53 @@ class Interface:
         self.stdscr.refresh()
         self.stdscr.getch()
         return
+
+    def animacao_inicio(self):
+        # vou fazer uma matriz escrito campo minado e imprimir pra ficar como se fossem as letras so que grandes
+        m_campo = [
+                [0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1,0,0,0,0,1,1,1,0,0],
+                [0,1,1,0,0,1,1,0,0,0,1,1,1,0,0,0,0,1,1,0,1,1,0,0,1,1,0,0,0,1,1,0,0,1,1,0,1,1,0],
+                [1,1,0,0,0,0,0,0,0,1,1,0,1,1,0,0,0,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0,1,1,0,0,0,1,1],
+                [1,1,0,0,0,0,0,0,0,1,1,0,1,1,0,0,1,1,0,1,0,1,1,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1],
+                [0,1,1,0,0,1,1,0,1,1,1,1,1,1,1,0,1,1,0,1,0,1,1,0,1,1,0,0,0,0,0,0,0,1,1,0,1,1,0],
+                [0,0,1,1,1,1,0,0,1,1,0,0,0,1,1,0,1,1,0,1,0,1,1,0,1,1,0,0,0,0,0,0,0,0,1,1,1,0,0]
+                ]
+        m_minado = [
+                [0,1,0,0,0,1,0,0,1,1,0,1,1,0,0,0,1,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,0,0],
+                [0,1,1,0,1,1,0,0,1,1,0,1,1,1,0,0,1,1,0,0,0,1,1,1,0,0,0,1,1,0,0,1,1,0,0,0,1,1,0,1,1,0],
+                [0,1,1,1,1,1,0,0,1,1,0,1,1,1,1,0,1,1,0,0,1,1,0,1,1,0,0,1,1,0,0,0,1,1,0,1,1,0,0,0,1,1],
+                [1,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,0,0,1,1,0,1,1,0,0,1,1,0,0,0,1,1,0,1,1,0,0,0,1,1],
+                [1,1,0,1,0,1,1,0,1,1,0,1,1,0,0,1,1,1,0,1,1,1,1,1,1,1,0,1,1,0,0,1,1,0,0,0,1,1,0,1,1,0],
+                [1,1,0,1,0,1,1,0,1,1,0,1,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0,1,1,1,1,1,0,0,0,0,0,1,1,1,0,0]
+                ]
+
+
+        self.stdscr.clear()
+
+        for lin in range(len(m_campo)):
+            for col in range(len(m_campo[lin])):
+                p = m_campo[lin][col]
+                if p == 1:
+                    self.stdscr.addstr((curses.LINES // 2) - 7 + lin, ((curses.COLS - len(m_campo[lin])) // 2) + col, " ", curses.A_REVERSE) # coloquei reverse mas era melhor que fosse cor eu acho
+                else:
+                    self.stdscr.addstr((curses.LINES // 2) - 7 + lin, ((curses.COLS - len(m_campo[lin])) // 2) + col, " ")
+
+        for lin in range(len(m_minado)):
+            for col in range(len(m_minado[lin])):
+                p = m_minado[lin][col]
+                if p == 1:
+                    self.stdscr.addstr((curses.LINES // 2) + lin, ((curses.COLS - len(m_minado[lin])) // 2) + col, " ", curses.A_REVERSE) # coloquei reverse mas era melhor que fosse cor eu acho
+                else:
+                    self.stdscr.addstr((curses.LINES // 2) + lin, ((curses.COLS - len(m_minado[lin])) // 2) + col, " ")
+
+        rodape = "aperte qualquer tecla para iniciar"
+        self.stdscr.addstr(curses.LINES - 3, (curses.COLS - len(rodape)) // 2, rodape)
+
+        self.stdscr.refresh()
+        self.stdscr.getch()
+
+
+
 
     def game_over(self):
         self.stdscr.clear()
